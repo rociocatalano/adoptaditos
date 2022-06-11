8255b213de20f3f374f353060f97696d32201b5e
Enki0189 <marudiaz0189@gmail.com>
2022-06-11 14:38:14 -0300

    se agrega json con mascotas

---
+++ dataccess/mascotas.js
@@ -0,0 +1,37 @@
+let Mascotas = [
+  {
+    id: 1,
+    nombre: "juan",
+    raza: "gato",
+    edad: 3,
+    tamaño: "mediano",
+    zona: "Devoto",
+    especial: false,
+    sexo: "M",
+    compatibilidad: "gato"
+  },
+  {
+    id: 2,
+    nombre: "Enki",
+    raza: "gato",
+    edad: 1,
+    tamaño: "pequeño",
+    zona: "Paternal",
+    especial: false,
+    sexo: "F",
+    compatibilidad: "gato, perro"
+  },
+  {
+    id: 3,
+    nombre: "Arya",
+    raza: "perro",
+    edad: 5,
+    tamaño: "grande",
+    zona: "Villa del Parque",
+    especial: true,
+    sexo: "F",
+    compatibilidad: "perro"
+  }
+];
+
+module.exports = { Mascotas };
