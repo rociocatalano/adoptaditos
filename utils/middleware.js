3c942c04e5ebbaecb61e31bedd98d293fc85eef7
Rocío Catalano <rociocatalano4@hotmail.com>
2022-07-03 17:25:12 -0300

    modificando unos detalles para el login

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,13 +1,34 @@
+require('dotenv').config();
+
 const consoleData = (req, res, next) => {
-  console.log("Method:" + req.method);
-  console.log("Path:" + req.path);
-  console.log("Body:", req.body);
-  console.log("------");
-  next();
+    console.log("Method:" + req.method);
+    console.log("Path:" + req.path);
+    console.log("Body:", req.body);
+    console.log("------");
+    next();
 };
 
 const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: "unknown endpoint" });
+    res.status(404).send({ error: "unknown endpoint" });
 };
 
-module.exports = { consoleData, unknownEndpoint };
+const processToken = (req, res, next) => {
+    const authorization = req.get('authorization');
+    if (authorization && authorization.toLowerCase().startsWith('bearer')) {
+        req.token = authorization.substring(7);
+    } else {
+        req.token = null;
+    }
+    next();
+}
+
+const validarUserLogin = (req, res, next) => {
+    const decodeToken = jwt.verify(req.token, process.env.JWTSECRET);
+    if (!req.token || !decodeToken.id) {
+        return res.status(401).json({ error: 'token missing or invalid' })
+    }
+    req.user = decodeToken;
+    next();
+}
+
+module.exports = { consoleData, unknownEndpoint, processToken };
