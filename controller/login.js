477e9872639cb221d870e647c6af5436bfd46194
Neus <onamonzon.32@gmail.com>
2022-07-03 15:56:43 -0300

    agrego login y carpeta request

---
+++ controller/login.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const router = require('express').Router();
+require('dotenv').config()
+
+router.post('/', (req,res)=>{
+  const {body} = req
+  if(body.username == 'refugio' && body.password == '123456'){
+    //usuario correcto
+    const tokenData = {
+      username: body.username,
+      id: 1,
+      profile: 'refugio'  
+    }
+
+    const token = jwt.sign(tokenData,process.env.JWTSECRET,{ expiresIn: '1h' })
+    res.status(200).send({token, name: 'regugio villa pueyrredon'})
+  }else{
+    //usuario incorrecto
+    return res.status(401).json({error:'credenciales incorrecto'})
+  }
+})
+
+module.exports = router
