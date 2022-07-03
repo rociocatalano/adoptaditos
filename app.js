59fccfafa7bf99bc12b061f77d0724d426a5d914
Rocío Catalano <rociocatalano4@hotmail.com>
2022-07-03 20:08:25 -0300

    modificación a app

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(middleware.consoleData);
+app.use(middleware.processToken);
 
 app.use("/mascotas", mascotasController);
 app.use("/login", loginController);
