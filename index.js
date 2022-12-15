1614d988888fc3729ad4e2707dc1ddbd466d2107
Neus <onamonzon.32@gmail.com>
2022-12-15 20:57:10 -0300

    new changes and add conect to BD

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const http = require("http");
 const app = require("./app");
+const { connectDb, sequelize } = require("./utils/db")
 
 const server = http.createServer(app);
+
+connectDb()
+
+sequelize.sync().then(() => {
+  console.log('Conectado a la BD correctamente')
+}).catch((error) => {
+  console.log('error', error)
+})
 
 server.listen(8000, () => {
   console.log("server running");
---
+++ utils/db.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+const Sequelize = require('sequelize')
+
+const sequelize = new Sequelize(
+    "adoptaditos", //nombre de la base de datos
+    "root", //usuario de la base de datos
+    "BasePack8", //contraseña de la base de datos
+    {
+        host: "localhost",
+        port: "3306",
+        dialect: 'mysql'
+    }
+)
+
+const connectDb = async() => {
+    try {
+        await sequelize.authenticate()
+        console.log('conectado a la base de datos')
+    } catch (error) {
+        console.log('ERROR:', error)
+        return process.exit()
+    }
+}
+
+module.exports = { connectDb, sequelize }
