54e6df7e97157c2f17a763e92e6dd4e4d6c629fc
Enki0189 <marudiaz0189@gmail.com>
2022-12-19 22:28:07 -0300

    Se agrega modelo de tabla usuarios-adoptantes.

---
+++ models/usuarios-adoptantes.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize')
+const { sequelize } = require('../utils/db')
+
+class UsuariosAdoptantes extends Model {}
+
+UsuariosAdoptantes.init({
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    apellido: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    provincia: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    localidad: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    mail: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    telefono: {
+        type: DataTypes.STRING(200),
+        allowNull: false
+    },
+    puntuacion: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+    },
+}, {
+    sequelize,
+    underscored: true,
+    modelName: 'usuarios-adoptantes'
+})
+
+module.exports = UsuariosAdoptantes
